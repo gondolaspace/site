432824a300f373f1cfdcdf6b8aa70e04dcde3c4b
Fraser Thompson <fraserthompson0@gmail.com>
2020-08-15 16:14:40 +1200

    Simplify favicon script and add max width

--- javascript/favicon.js
+++ javascript/favicon.js
@@ -1,67 +1,94 @@
 /* http://mit-license.org/ */
 var faviconId = "gondola-favicon-link";
-(function (root, factory) {
-    if (typeof define === "function" && define["amd"]) {
-        // AMD. Register as an anonymous module.
-        define([], factory);
-    } else if (typeof module === "object" && module["exports"]) {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        module["exports"] = factory();
-    } else {
-        // Browser globals (root is window)
-        root["favicon"] = factory();
-  }
-}(typeof self !== "undefined" ? self : this, function () {
-    var doc = document;
-    // private
+var faviconEl =  document.getElementById(faviconId)
 
-    var head = doc.getElementsByTagName("head")[0];
-    var loopTimeout = null;
-    var changeFavicon = function(iconURL) {
-        // var newLink = doc.createElement("link");
-        // newLink.type = "image/x-icon";
-        // newLink.rel = "icon";
-        // newLink.href = iconURL;
-        // newLink.id = faviconId;
-        // doc.getElementById(faviconId).remove()
-        // head.appendChild(newLink);
-        doc.getElementById(faviconId).href = iconURL;
-    };
+var loopTimeout = null;
 
-    // public
+var changeFavicon = function(iconURL) {
+    faviconEl.href = iconURL;
+};
 
-    var favicon = {
-        "defaultPause": 2000,
-        "change": function(iconURL, optionalDocTitle) {
-            clearTimeout(loopTimeout);
-            if (optionalDocTitle) {
-                doc.title = optionalDocTitle;
-            }
-            if (iconURL !== "") {
-                changeFavicon(iconURL);
-            }
-        },
-        "animate": function(icons, optionalDelay) {
-            clearTimeout(loopTimeout);
-            // preload icons
-            icons.forEach(function(icon) {
-                (new Image()).src = icon;
-            });
-            optionalDelay = optionalDelay || this["defaultPause"];
-            var iconIndex = 0;
+var favicon = {
+    "defaultPause": 2000,
+    "change": function(iconURL, optionalDocTitle) {
+        clearTimeout(loopTimeout);
+        if (optionalDocTitle) doc.title = optionalDocTitle;
+        if (iconURL !== "") changeFavicon(iconURL);
+    },
+    "animate": function(icons, optionalDelay) {
+        clearTimeout(loopTimeout);
+
+        // preload icons
+        icons.forEach(function(icon) {
+            (new Image()).src = icon;
+        });
+
+        optionalDelay = optionalDelay || this["defaultPause"];
+
+        var iconIndex = 0;
+        changeFavicon(icons[iconIndex]);
+
+        loopTimeout = setTimeout(function animateFunc() {
+            iconIndex = (iconIndex + 1) % icons.length;
             changeFavicon(icons[iconIndex]);
-            loopTimeout = setTimeout(function animateFunc() {
-                iconIndex = (iconIndex + 1) % icons.length;
-                changeFavicon(icons[iconIndex]);
-                loopTimeout = setTimeout(animateFunc, optionalDelay);
-            }, optionalDelay);
-        },
-        "stopAnimate": function() {
-            clearTimeout(loopTimeout);
-        }
-    };
+            loopTimeout = setTimeout(animateFunc, optionalDelay);
+        }, optionalDelay);
+    },
+    "stopAnimate": function() {
+        clearTimeout(loopTimeout);
+    }
+};
 
-    return favicon;
-}));
+favicon.animate([
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (1).ico",
+    "favicon/dancing-gondola (2).ico",
+    "favicon/dancing-gondola (2).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (3).ico",
+    "favicon/dancing-gondola (4).ico",
+    "favicon/dancing-gondola (4).ico",
+    "favicon/dancing-gondola (5).ico",
+    "favicon/dancing-gondola (5).ico",
+    "favicon/dancing-gondola (6).ico",
+    "favicon/dancing-gondola (6).ico",
+    "favicon/dancing-gondola (7).ico",
+    "favicon/dancing-gondola (7).ico",
+    "favicon/dancing-gondola (8).ico",
+    "favicon/dancing-gondola (9).ico",
+    "favicon/dancing-gondola (9).ico",
+    "favicon/dancing-gondola (10).ico",
+    "favicon/dancing-gondola (10).ico",
+    "favicon/dancing-gondola (10).ico",
+    "favicon/dancing-gondola (10).ico",
+    "favicon/dancing-gondola (10).ico",
+    "favicon/dancing-gondola (10).ico",
+    "favicon/dancing-gondola (11).ico",
+    "favicon/dancing-gondola (11).ico",
+    "favicon/dancing-gondola (12).ico",
+    "favicon/dancing-gondola (12).ico",
+    "favicon/dancing-gondola (13).ico",
+    "favicon/dancing-gondola (13).ico",
+    "favicon/dancing-gondola (14).ico",
+    "favicon/dancing-gondola (14).ico"
+ ], 60)
